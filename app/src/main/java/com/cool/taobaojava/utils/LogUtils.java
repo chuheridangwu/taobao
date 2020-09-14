35ec3acc1575b0636d835978ca6b7ed62e659e91
dongyumao@tiange.com <dongyumao@tiange.com>
2020-09-14 17:58:34 +0800

    1、 添加log日志工具,小写更改成大写

--- app/src/main/java/com/cool/taobaojava/utils/LogUtils.java
+++ app/src/main/java/com/cool/taobaojava/utils/LogUtils.java
@@ -4,31 +4,31 @@ import android.util.Log;
 
 public class LogUtils {
     private static int currentLev = 4;
-    private static int debugLev = 4;
-    private static int infoLev = 3;
-    private static int warningLev = 2;
-    private static int errorLev = 1;
+    private static final int DEBUG_LEV = 4;
+    private static final int INFO_LEV = 3;
+    private static final int WARNING_LEV = 2;
+    private static final int ERROR_LEV = 1;
 
     public static void d(Class clazz,String log){
-        if (currentLev >= debugLev){
+        if (currentLev >= DEBUG_LEV){
             Log.d(clazz.getSimpleName(), log);
         }
     }
 
     public static void i(Class clazz,String log){
-        if (currentLev >= infoLev){
+        if (currentLev >= INFO_LEV){
             Log.i(clazz.getSimpleName(), log);
         }
     }
 
     public static void w(Class clazz,String log){
-        if (currentLev >= warningLev){
+        if (currentLev >= WARNING_LEV){
             Log.w(clazz.getSimpleName(), log);
         }
     }
 
     public static void e(Class clazz,String log){
-        if (currentLev >= errorLev){
+        if (currentLev >= ERROR_LEV){
             Log.e(clazz.getSimpleName(), log);
         }
     }

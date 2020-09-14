7b2003347eb6e647f08610ad7dacf7b2b973a979
dongyumao@tiange.com <dongyumao@tiange.com>
2020-09-14 17:53:55 +0800

    1、 添加log日志工具

--- app/src/main/java/com/cool/taobaojava/ui/activity/MainActivity.java
+++ app/src/main/java/com/cool/taobaojava/ui/activity/MainActivity.java
@@ -1,15 +1,23 @@
 package com.cool.taobaojava.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.cool.taobaojava.R;
 import com.cool.taobaojava.ui.fragment.HomeFragment;
+import com.cool.taobaojava.utils.LogUtils;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +25,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initView();
+
+        initListener();
+    }
+
+    private void initListener() {
+        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        LogUtils.d(MainActivity.class,"首页");
+                        break;
+                    case R.id.selected:
+                        LogUtils.i(MainActivity.class,"选中");
+                        break;
+                    case R.id.red_packet:
+                        LogUtils.e(MainActivity.class,"红包");
+                        break;
+                    case R.id.search:
+                        LogUtils.w(MainActivity.class,"搜索");
+                        break;
+                }
+
+                return true;
+            }
+        });
     }
 
     private void initView() {
+        mNavigationView = findViewById(R.id.main_navigation_bar);
+
         // 创建fragment
         HomeFragment home = new HomeFragment();
         // 获取fragment管理器
---
+++ app/src/main/java/com/cool/taobaojava/utils/LogUtils.java
@@ -0,0 +1,36 @@
+package com.cool.taobaojava.utils;
+
+import android.util.Log;
+
+public class LogUtils {
+    private static int currentLev = 4;
+    private static int debugLev = 4;
+    private static int infoLev = 3;
+    private static int warningLev = 2;
+    private static int errorLev = 1;
+
+    public static void d(Class clazz,String log){
+        if (currentLev >= debugLev){
+            Log.d(clazz.getSimpleName(), log);
+        }
+    }
+
+    public static void i(Class clazz,String log){
+        if (currentLev >= infoLev){
+            Log.i(clazz.getSimpleName(), log);
+        }
+    }
+
+    public static void w(Class clazz,String log){
+        if (currentLev >= warningLev){
+            Log.w(clazz.getSimpleName(), log);
+        }
+    }
+
+    public static void e(Class clazz,String log){
+        if (currentLev >= errorLev){
+            Log.e(clazz.getSimpleName(), log);
+        }
+    }
+
+}
